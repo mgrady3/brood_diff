d45988fe68a5cd47ad176d287c2c081fc5a1b391
Maxwell Grady <max.grady@gmail.com>
2018-08-20 17:44:00 -0500

    Add tests for get index, and basic io

--- brood_diff/tests/test_get_indices.py
+++ brood_diff/tests/test_get_indices.py
@@ -1,2 +1,43 @@
-import requests
+from brood_diff.brood_diff import get_index
 import pytest
+import requests
+
+
+class TestGetIndex(object):
+    """ Test getting json/dict index of a Brood instance."""
+
+    def test_get_index_bad_url(self):
+        # given
+        BASE_URL = "https://packages.entought.com"  # typo
+        REPO = "gpl"
+        ORG = "enthought"
+        PLAT = "rh6-x86_84"
+        VER = "cp27"
+
+        # when
+        with pytest.raises(requests.exceptions.ConnectionError) as execinfo:
+            get_index(url=BASE_URL,
+                      org=ORG,
+                      repo=REPO,
+                      plat=PLAT,
+                      pyver=VER)
+        # then
+        assert "Failed to establish a new connection" in str(execinfo.value)
+
+    def test_get_index(self):
+        # given
+        BASE_URL = "https://packages.enthought.com"
+        REPO = "gpl"
+        ORG = "enthought"
+        PLAT = "rh6-x86_84"
+        VER = "cp27"
+
+        # when
+        idx = get_index(url=BASE_URL,
+                        org=ORG,
+                        repo=REPO,
+                        plat=PLAT,
+                        pyver=VER)
+        # then
+        assert isinstance(idx, dict)
+        assert idx
---
+++ brood_diff/tests/test_io.py
@@ -0,0 +1,67 @@
+from brood_diff.brood_diff import get_index, to_json_file, from_json_file
+
+import os
+import tempfile
+
+
+class TestIO(object):
+    # potentially useful paths for tests
+    thisdir = os.path.abspath(os.path.dirname(__file__))
+    srcdir = os.path.abspath(os.path.join(thisdir, os.pardir))
+    rootdir = os.path.abspath(os.path.join(srcdir, os.pardir))
+    test_data = os.path.join(rootdir, "test_data")
+
+    def test_to_json_file(self):
+        # given
+        BASE_URL = "https://packages.enthought.com"
+        REPO = "gpl"
+        ORG = "enthought"
+        PLAT = "rh6-x86_84"
+        VER = "cp27"
+
+        # when
+        idx = get_index(url=BASE_URL,
+                        org=ORG,
+                        repo=REPO,
+                        plat=PLAT,
+                        pyver=VER)
+        _, path = tempfile.mkstemp(suffix=".json")
+        to_json_file(idx, path)
+
+        # then
+        assert os.path.exists(path)
+        assert os.path.getsize(path) > 0
+
+    def test_from_json(self):
+        # given
+        test_file = os.path.join(self.test_data, "idx-8-20-18-e-gpl-36.json")
+
+        # when
+        idx = from_json_file(test_file)
+
+        # then
+        assert isinstance(idx, dict)
+        assert idx
+
+    def test_to_from_json_round_trip(self):
+        # given
+        BASE_URL = "https://packages.enthought.com"
+        REPO = "gpl"
+        ORG = "enthought"
+        PLAT = "rh6-x86_84"
+        VER = "cp27"
+
+        # when
+        idx_from_brood = get_index(url=BASE_URL,
+                                   org=ORG,
+                                   repo=REPO,
+                                   plat=PLAT,
+                                   pyver=VER)
+
+        _, path = tempfile.mkstemp(suffix=".json")
+        to_json_file(idx_from_brood, path)
+
+        idx_from_file = from_json_file(path)
+
+        # then
+        assert idx_from_brood == idx_from_file
