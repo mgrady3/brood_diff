6059ba0c52424fccd7b6cd3e8c7c1b662c4201e6
Maxwell Grady <max.grady@gmail.com>
2018-08-22 16:42:38 -0500

    Add file to hold sets of valid command line args

---
+++ brood_diff/valid.py
@@ -0,0 +1,20 @@
+# (C) Copyright 2018 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+
+PLATS = ["osx-x86",
+         "rh5-x86",
+         "win-x86",
+         "osx-x86_64",
+         "rh5-x86_64",
+         "win-x86_64",
+         "rh6-x86",
+         "rh6-x86_64",
+         "rh7-x86",
+         "rh7-x86_64"]
+
+VERS = ["cp27",
+        "cp34",
+        "cp35",
+        "cp36",
+        "pp27"]
