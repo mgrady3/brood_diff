5ec4d36795cda4fd656eec42081aa9501c8e8959
Maxwell Grady <max.grady@gmail.com>
2018-08-28 17:24:16 -0500

    Bump version for most recent changes.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brood_diff',
-    version='0.2.0',
+    version='0.2.5',
     url='',
     author='Maxwell Grady',
     author_email='mgrady@enthought.com',
