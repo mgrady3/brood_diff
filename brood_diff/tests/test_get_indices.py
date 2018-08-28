ccf34ad98ffd8efb566d998e352de7e8a4dcefde
Maxwell Grady <max.grady@gmail.com>
2018-08-28 16:47:20 -0500

    Add extra tests for bad plat + bad ver

--- brood_diff/tests/test_get_indices.py
+++ brood_diff/tests/test_get_indices.py
@@ -61,7 +61,49 @@ class TestGetIndex(object):
                       plat=PLAT,
                       pyver=VER)
         # then
-        assert "404" in str(execinfo.value)
+        assert "Client Error" in str(execinfo.value)  # should be 404
+
+    def test_get_index_bad_plat(self):
+        """ Note: CLI validates plat + ver so this shouldn't happen when
+        using the CLI but could happen if calling get_index directly.
+        """
+        # given
+        BASE_URL = "https://packages.enthought.com"
+        REPO = "gpl"
+        ORG = "enthought"
+        PLAT = "rh6-x87_64"  # intentional typo
+        VER = "cp27"
+
+        # when
+        with pytest.raises(requests.HTTPError) as execinfo:
+            get_index(url=BASE_URL,
+                      org=ORG,
+                      repo=REPO,
+                      plat=PLAT,
+                      pyver=VER)
+        # then
+        assert "Client Error" in str(execinfo.value)  # should be 404
+
+    def test_get_index_bad_ver(self):
+        """ Note: CLI validates plat + ver so this shouldn't happen when
+        using the CLI but could happen if calling get_index directly.
+        """
+        # given
+        BASE_URL = "https://packages.enthought.com"
+        REPO = "gpl"
+        ORG = "enthought"
+        PLAT = "rh6-x86_64"
+        VER = "cp17"  # intentional typo
+
+        # when
+        with pytest.raises(requests.HTTPError) as execinfo:
+            get_index(url=BASE_URL,
+                      org=ORG,
+                      repo=REPO,
+                      plat=PLAT,
+                      pyver=VER)
+        # then
+        assert "Client Error" in str(execinfo.value)  # should be 400
 
     def test_legacy_get_index(self):
         # given
