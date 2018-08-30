397504abf4b3306bcfde0707697b7d5bfcf0d1ff
Maxwell Grady <max.grady@gmail.com>
2018-08-29 19:08:26 -0500

    Add basic validation functions.

--- brood_diff/valid.py
+++ brood_diff/valid.py
@@ -2,6 +2,7 @@
 # All rights reserved.
 #
 
+
 PLATS = ["osx-x86",
          "rh5-x86",
          "win-x86",
@@ -18,3 +19,18 @@ VERS = ["cp27",
         "cp35",
         "cp36",
         "pp27"]
+
+
+def validate_platform(plat: str):
+    """ Validate User CLI input."""
+    return plat in PLATS
+
+
+def validate_version(ver: str):
+    """ Validate User CLI input."""
+    return ver in VERS
+
+
+def validate_org_repo(org_repo: str):
+    """ Validate User CLI input."""
+    return "/" in org_repo and len(org_repo.split("/") == 2)
