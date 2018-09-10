31ff71d03c6121ae1061d8dc3c6bc5381b7cb3ce
Maxwell Grady <max.grady@gmail.com>
2018-09-10 17:01:01 -0500

    Update to new click callback API

--- brood_diff/valid.py
+++ brood_diff/valid.py
@@ -23,35 +23,41 @@ VERS = ["cp27",
         "pp27"]
 
 
-def validate_platform(ctx: click.Context, plat: str):
+def validate_platform(ctx: click.Context,
+                      param: click.core.Option,
+                      value: str):
     """ Validate User CLI input."""
-    if plat in PLATS:
-        return plat
+    if value in PLATS:
+        return value
     else:
         raise click.BadParameter(
             ("Invalid platform: {}. Please use list-platforms for a list of"
-             " supported platforms.".format(plat)))
+             " supported platforms.".format(value)))
 
 
-def validate_version(ctx: click.Context, ver: str):
+def validate_version(ctx: click.Context,
+                     param: click.core.Option,
+                     value: str):
     """ Validate User CLI input."""
-    if ver in VERS:
-        return ver
+    if value in VERS:
+        return value
     else:
         raise click.BadParameter(
             ("Invalid python version: {}. Please use list-platforms for a list"
-             " of supported platforms.".format(ver)))
+             " of supported platforms.".format(value)))
 
 
-def validate_org_repo(ctx: click.Context, org_repo: str):
+def validate_org_repo(ctx: click.Context,
+                      param: click.core.Option,
+                      value: str):
     """ Validate User CLI input.
     Repositories are formatted using the EDS/Hatcher format <org/repo> - e.g.
         enthought/free
     """
-    if "/" in org_repo and len(org_repo.split("/")) == 2:
-        return org_repo
+    if "/" in value and len(value.split("/")) == 2:
+        return value
     else:
         raise click.BadParameter(
             ("Invalid repository format: {}. Repositories must use the"
              " EDS/Hatcher format <org/repo>."
-             "e.g. enthought/free".format(org_repo)))
+             "e.g. enthought/free".format(value)))
