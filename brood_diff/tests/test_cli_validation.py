a8f09b6dc01b38cb839e34d6a250333fc5237d25
Maxwell Grady <max.grady@gmail.com>
2018-09-10 15:57:12 -0500

    Add tests for version and repo validation.

--- brood_diff/tests/test_cli_validation.py
+++ brood_diff/tests/test_cli_validation.py
@@ -21,8 +21,39 @@ class TestValidation(object):
 
         with pytest.raises(click.BadParameter) as execinfo:
             valid.validate_platform(ctx, bad_plat)
-        
+
         # then
         assert g_plat == good_plat
         assert "Invalid platform" in str(execinfo.value)
-        
+
+    def test_validate_version(self):
+        # given
+        good_vers = "cp36"
+        bad_vers = "cp16"
+        ctx = None
+
+        # when
+        g_vers = valid.validate_version(ctx, good_vers)
+
+        with pytest.raises(click.BadParameter) as execinfo:
+            valid.validate_version(ctx, bad_vers)
+
+        # then
+        assert g_vers == good_vers
+        assert "Invalid python version" in str(execinfo.value)
+
+    def test_validate_repository(self):
+        # given
+        good_repo = "enthought/free"
+        bad_repo = "enthought free"
+        ctx = None
+
+        # when
+        g_repo = valid.validate_org_repo(ctx, good_repo)
+
+        with pytest.raises(click.BadParameter) as execinfo:
+            valid.validate_org_repo(ctx, bad_repo)
+
+        # then
+        assert g_repo == good_repo
+        assert "Invalid repository" in str(execinfo.value)
