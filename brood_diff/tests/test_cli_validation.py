6f323dc6ecac4dcf38b32bb76ca00bd2b9609693
Maxwell Grady <max.grady@gmail.com>
2018-09-11 15:31:05 -0500

    Add new tests for multi-value validation funcs.

--- brood_diff/tests/test_cli_validation.py
+++ brood_diff/tests/test_cli_validation.py
@@ -10,6 +10,23 @@ from brood_diff import valid
 class TestValidation(object):
     """ Testing of CLI option validation."""
 
+    def test_validate_platforms(self):
+        good_plats = ("osx-x86_64", "rh6-x86_64", "win-x86_64")
+        bad_plats = ("osx-x87_64", "rh6-x86_64", "win-x86_64")
+        ctx = None
+        param = None
+
+        # when
+        g_plats = valid.validate_platforms(ctx, param, good_plats)
+
+        with pytest.raises(click.BadParameter) as execinfo:
+            valid.validate_platforms(ctx, param, bad_plats)
+
+        # then
+        assert g_plats == good_plats
+        assert "Invalid platform(s)" in str(execinfo.value)
+        assert "osx-x87_64" in str(execinfo.value)
+
     def test_validate_platform(self):
         # given
         good_plat = "osx-x86_64"
@@ -27,6 +44,24 @@ class TestValidation(object):
         assert g_plat == good_plat
         assert "Invalid platform" in str(execinfo.value)
 
+    def test_validate_versions(self):
+        # given
+        good_vers = ("cp36", "cp35", "cp27")
+        bad_vers = ("cp16", "cp35", "cp27")
+        ctx = None
+        param = None
+
+        # when
+        g_vers = valid.validate_versions(ctx, param, good_vers)
+
+        with pytest.raises(click.BadParameter) as execinfo:
+            valid.validate_versions(ctx, param, bad_vers)
+
+        # then
+        assert g_vers == good_vers
+        assert "Invalid python version(s)" in str(execinfo.value)
+        assert "cp16" in str(execinfo.value)
+
     def test_validate_version(self):
         # given
         good_vers = "cp36"
@@ -43,6 +78,24 @@ class TestValidation(object):
         # then
         assert g_vers == good_vers
         assert "Invalid python version" in str(execinfo.value)
+
+    def test_validate_repositories(self):
+        # given
+        good_repos = ("enthought/free", "enthought/gpl", "an_org/a_repo")
+        bad_repos = ("enthought free",  "enthought/gpl", "an_org/a_repo")
+        ctx = None
+        param = None
+
+        # when
+        g_repos = valid.validate_org_repos(ctx, param, good_repos)
+
+        with pytest.raises(click.BadParameter) as execinfo:
+            valid.validate_org_repos(ctx, param, bad_repos)
+
+        # then
+        assert g_repos == good_repos
+        assert "Invalid repository" in str(execinfo.value)
+        assert "enthought free" in str(execinfo.value)
 
     def test_validate_repository(self):
         # given
